f550828dce045ae00e69519cbb6b8980e35bab63
Nikita Turgenev <turgenerator@gmail.com>
2024-03-03 15:29:14 +0300

    first commit

---
+++ M16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  M16
+//
+//  Created by Nikita Turgenev on 03.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ M16UITests/M16UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  M16UITestsLaunchTests.swift
+//  M16UITests
+//
+//  Created by Nikita Turgenev on 03.03.2024.
+//
+
+import XCTest
+
+final class M16UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
